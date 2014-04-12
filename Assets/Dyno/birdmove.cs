1fc305d440156e9c6e3c095a73415557513bc116
David Arias <dariaspr@gmail.com>
2014-04-12 10:28:33 -0400

    Initial Commit
    Initial commit of all sources

---
+++ Assets/Dyno/birdmove.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class birdmove : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	public Vector3 gravity;
+	public Vector3 FlapVelocity;
+	public float maxSpeed = 25f;
+	private AudioSource dinoDieSound;
+	private AudioSource dinoFartSound;
+	protected Animator animator;
+
+	private GUIText textfield;
+	private int score;
+
+
+	bool didFlap = false;
+
+	// Use this for initialization
+	void Start () {
+		dinoDieSound = (AudioSource)GameObject.Find ("DynoDieSound").GetComponent<AudioSource> ();
+		dinoFartSound = (AudioSource)GameObject.Find ("DynoFartSound").GetComponent<AudioSource> ();
+		//textfield = (GUIText)GameObject.Find("txt-score").GetComponent<GUIText> ();
+		//score = 3;
+
+		animator = GetComponent<Animator>();
+	
+	}
+
+	void Update() {
+
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButton(0)) {
+					didFlap = true;
+			}
+			if (Input.GetKeyDown (KeyCode.F)) {
+				dinoFartSound.Play();
+			}
+
+		}
+
+
+
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+	
+		if (!animator.GetBool ("Die")) {
+						velocity += gravity * Time.deltaTime;
+
+						if (didFlap == true) {
+								didFlap = false;
+								velocity += FlapVelocity;
+
+						}
+
+						velocity = Vector3.ClampMagnitude (velocity, maxSpeed); 
+
+						transform.position += velocity * Time.deltaTime;
+
+						float angle = 10;
+
+						if (velocity.y < 0) {
+								angle = Mathf.LerpAngle (0, -15, Time.time);
+						}
+
+						//transform.eulerAngles = Vector3(0, angle, 0);
+						transform.rotation = Quaternion.Euler (0, 0, angle);
+
+	
+				}
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (!animator.GetBool ("Die")) {
+
+			score -= 1;
+			//textfield.text  = score.ToString();
+
+		if (col.gameObject.tag == "bird") {
+				rigidbody2D.gravityScale=30;
+
+						//renderer.enabled = false;
+						dinoDieSound.Play();
+						transform.Rotate(0,0,180);
+					//GetComponent<Animator>().SetTrigger(
+						//Destroy(gameObject);
+					animator.SetBool("Die", true );
+				}
+		}
+	}
+}
---
+++ Assets/Police/HorizontalMove.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HorizontalMove : MonoBehaviour {
+
+	//#if UNITY_EDITOR
+	//Debug.Log ("El  valor de...");
+	//#endif
+
+	public float speed = 0.3f;
+	public Direction direction;
+
+	public enum Direction {
+		LEFT = 1,
+		NONE = 0,
+		RIGHT = -1
+	};
+		
+		// Update is called once per frame
+		void Update () {
+
+		var tmp = transform.position;
+		switch ((int)direction) {
+				case 1:
+		
+						if (transform.position.x > -110) {
+								tmp.x -= (speed);
+								transform.position = tmp;
+						} else {
+								tmp.x = 110;
+								transform.position = tmp;
+						}
+						break;
+		
+				case -1:
+		
+						if (transform.position.x < 110) {
+								tmp.x += (speed);
+								transform.position = tmp;
+						} else {
+								tmp.x = -110;
+								transform.position = tmp;
+						}
+						break;
+		
+				}
+
+
+	}
+
+
+}
